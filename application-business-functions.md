6127a08a3f349635d7299cdd426d0ba0c34c30e6
Anthony Coates <abcoates@contakt.org>
2017-09-05 18:03:03 +0100

    Have now implemented a Python script that documents the 'facts' from an Archi model as text in tables in Markdown format.

---
+++ application-business-functions.md
@@ -0,0 +1,70 @@
+# **Archimate Model Facts**
+| Property | Value |
+| ---- | ---- |
+| **Model Name** | application-business-functions |
+| **Model ID** | id-306bc760-438d-4cdf-b1ff-a11476e3dcd2 |
+
+## <a name='id-df026a90-372e-4928-9940-3402a6cbe955'></a>**Business Function:** L1 Business Function
+| Property | Value |
+| ---- | ---- |
+| **Name** | L1 Business Function |
+| **Type** | Business Function |
+| **Documentation** | Documentation for L1 business function.<br><br>Here is some further documentation. |
+| **ID** | id-df026a90-372e-4928-9940-3402a6cbe955 |
+
+## <a name='id-17d58b02-fdec-4b8b-a8b1-34e43632db5a'></a>**Business Function:** L2 Business Function
+| Property | Value |
+| ---- | ---- |
+| **Name** | L2 Business Function |
+| **Type** | Business Function |
+| **Documentation** |  |
+| **ID** | id-17d58b02-fdec-4b8b-a8b1-34e43632db5a |
+
+| Relationship Type | Relationship Name | Target Name | Target Type | Relationship Documentation |
+| ---- | ---- | ---- | ---- | ---- |
+| Specialization |  | L1 Business Function | Business Function |  |
+## <a name='id-31aac30e-236a-4c08-81da-3eb009204d4e'></a>**Business Function:** L3 Business Function
+| Property | Value |
+| ---- | ---- |
+| **Name** | L3 Business Function |
+| **Type** | Business Function |
+| **Documentation** |  |
+| **ID** | id-31aac30e-236a-4c08-81da-3eb009204d4e |
+
+| Relationship Type | Relationship Name | Target Name | Target Type | Relationship Documentation |
+| ---- | ---- | ---- | ---- | ---- |
+| Specialization |  | L2 Business Function | Business Function |  |
+## <a name='id-52ae7cea-9110-4e0a-92f2-de50f02bcd13'></a>**Application Component:** Application 1
+| Property | Value |
+| ---- | ---- |
+| **Name** | Application 1 |
+| **Type** | Application Component |
+| **Documentation** | First application.<br><br>Further documentation about first application. |
+| **ID** | id-52ae7cea-9110-4e0a-92f2-de50f02bcd13 |
+
+| Relationship Type | Relationship Name | Target Name | Target Type | Relationship Documentation |
+| ---- | ---- | ---- | ---- | ---- |
+| Realization | implements business function | L1 Business Function | Business Function | Application 1 implements the business function 'L1 Business Function'. |
+## <a name='id-5a2f4550-b9d8-4cf1-9ef7-745392b4464e'></a>**Application Component:** Application 2
+| Property | Value |
+| ---- | ---- |
+| **Name** | Application 2 |
+| **Type** | Application Component |
+| **Documentation** |  |
+| **ID** | id-5a2f4550-b9d8-4cf1-9ef7-745392b4464e |
+
+| Relationship Type | Relationship Name | Target Name | Target Type | Relationship Documentation |
+| ---- | ---- | ---- | ---- | ---- |
+| Realization |  | L2 Business Function | Business Function |  |
+## <a name='id-8ae75738-9c08-401c-8730-899893cc86b7'></a>**Application Component:** Application 3
+| Property | Value |
+| ---- | ---- |
+| **Name** | Application 3 |
+| **Type** | Application Component |
+| **Documentation** |  |
+| **ID** | id-8ae75738-9c08-401c-8730-899893cc86b7 |
+
+| Relationship Type | Relationship Name | Target Name | Target Type | Relationship Documentation |
+| ---- | ---- | ---- | ---- | ---- |
+| Realization |  | L2 Business Function | Business Function |  |
+| Realization |  | L3 Business Function | Business Function |  |
---
+++ archi-xml-to-markdown.py
@@ -0,0 +1,93 @@
+# Usage: python archi-xml-to-markdown.py <xml-input-path> <markdown-output-path>
+
+import sys
+import re
+import xml.etree.ElementTree as ET
+
+ns = {
+	'm': 'http://www.opengroup.org/xsd/archimate/3.0/',
+	'xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+	'xml': 'http://www.w3.org/XML/1998/namespace'
+}
+
+def makecurie(ns, prefix, localname):
+	return "{%s}%s" % (ns[prefix], localname)
+
+def elemtext(e):
+	return e.text if e is not None else ''
+
+xsitype = makecurie(ns, 'xsi', 'type')
+
+camelCaseRegex = re.compile(r"([a-z])([A-Z])")
+
+def escapetext(text):
+	return text.replace("\n", "<br>").replace("\r", "")
+
+# Read the file paths from the command-line.
+xmlInputPath = sys.argv[1]
+mdOutputPath = sys.argv[2]
+
+# Read input XML file.
+tree = ET.parse(xmlInputPath)
+root = tree.getroot()
+
+# Create output Markdown file.
+with open(mdOutputPath, mode='w', encoding='utf-8') as mdout:
+
+	# Write out model-level metadata.
+	mdout.writelines([
+		"# **Archimate Model Facts**\n",
+		"| Property | Value |\n",
+		"| ---- | ---- |\n",
+		"| **Model Name** | {0} |\n".format(elemtext(root.find('m:name', ns))),
+		"| **Model ID** | {0} |\n".format(root.attrib['identifier']),
+		"\n"
+	])
+
+	# Write out element properties.
+	elements = root.findall('m:elements/m:element', ns)
+	for element in elements:
+		elemtype = camelCaseRegex.sub(r"\1 \2", element.attrib[xsitype])
+		elemid = element.attrib['identifier']
+		mdout.writelines([
+			"## <a name='{0}'></a>**{1}:** {2}\n".format(elemid, elemtype, elemtext(element.find('m:name',ns))),
+			"| Property | Value |\n",
+			"| ---- | ---- |\n",
+			"| **Name** | {0} |\n".format(elemtext(element.find('m:name',ns))),
+			"| **Type** | {0} |\n".format(elemtype),
+			"| **Documentation** | {0} |\n".format(escapetext(elemtext(element.find('m:documentation',ns)))),
+			"| **ID** | {0} |\n".format(element.attrib['identifier']),
+			"\n"
+		])
+		relationships = root.findall("m:relationships/m:relationship[@source='%s']" % elemid, ns)
+		if (len(relationships) >= 1):
+			mdout.writelines([
+				"| Relationship Type | Relationship Name | Target Name | Target Type | Relationship Documentation |\n"
+				"| ---- | ---- | ---- | ---- | ---- |\n"
+			])
+			for relationship in relationships:
+				relntype = camelCaseRegex.sub(r"\1 \2", relationship.attrib[xsitype])
+				targetid = relationship.attrib['target']
+				target = root.findall("m:elements/m:element[@identifier='%s']" % targetid, ns)[0]
+				targettype = camelCaseRegex.sub(r"\1 \2", target.attrib[xsitype])
+				mdout.write(
+					"| {0} | {1} | {2} | {3} | {4} |\n".format(
+						relntype,
+						elemtext(relationship.find('m:name',ns)),
+						elemtext(target.find('m:name',ns)),
+						targettype,
+						escapetext(elemtext(relationship.find('m:documentation',ns)))
+					)
+				)
+				
+
+	# Write out relationship properties sheet.
+#	relationshipcolumns = ['relationship_type', 'relationship_identifier', 'relationship_source_identifier', 'relationship_target_identifier']
+#	relationships = root.findall('m:relationships/m:relationship', ns)
+#	relationshipsdf = pd.DataFrame({
+#		'relationship_type': list(map(lambda e: e.attrib[xsitype], relationships)),
+#		'relationship_identifier': list(map(lambda e: e.attrib['identifier'], relationships)),
+#		'relationship_source_identifier': list(map(lambda e: e.attrib['source'], relationships)),
+#		'relationship_target_identifier': list(map(lambda e: e.attrib['target'], relationships))
+#	})
+#	relationshipsdf.to_excel(writer, sheet_name='relationships', columns=relationshipcolumns, index=False)
